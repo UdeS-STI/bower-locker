6d93400730d3d8d8631084b516d643583a5541b4
Stéphane Leblond <stephane.leblond@usherbrooke.ca>
2018-07-10 10:15:45 -0400

    OPR-309 L'enregistrement ne fonctionne pas sous IE (#3)

--- bower-locker-lock.js
+++ bower-locker-lock.js
@@ -48,8 +48,12 @@ function lock(isVerbose) {
         // NOTE: Use dirName as the dependency name as it is more accurate than .bower.json properties
         var name = dep.dirName;
         var version = dep.type === 'branch' ? dep.branch : dep.release;
-        bowerConfig.dependencies[name] = dep.src + '#' + version; // _source
-        bowerConfig.resolutions[name] = version;
+        bowerConfig.dependencies[name] = dep.src + (version ? '#' + version: ''); // _source
+        if(!bowerConfig.resolutions[name]) {
+            bowerConfig.resolutions[name] = version ? version : '*';
+        } else if (bowerConfig.resolutions[name] !== '*') {
+            bowerConfig.resolutions[name] = version
+        }
         if (isVerbose) {
             console.log('  %s (%s): %s', name, dep.release, dep.commit);
         }
